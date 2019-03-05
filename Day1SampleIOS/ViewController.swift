781e3bdb92bf104c63f17f0cfb753003dc116dd9
Jubin <jubinks3@gmail.com>
2019-03-05 12:28:38 -0500

    Initial Commit

---
+++ Day1SampleIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day1SampleIOS
+//
+//  Created by Jubin KS on 2019-03-05.
+//  Copyright © 2019 Jubin KS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
